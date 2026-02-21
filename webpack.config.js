90affb89874e4600fcd075228cc16c57d784b29c
Kirill Shikov <rbhz2806@mail.ru>
2026-02-21 13:30:53 +0300

    домашнее задание 2

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+      rules: [
+         {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      })
+    ]
+}
